ee2700341a514bc2711cb59394780b896b8adeaa
SahilMj007 <sahilmj07@gmail.com>
2026-01-23 16:43:03 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
